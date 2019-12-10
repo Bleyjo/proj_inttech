00c51a88dee07da4fba93b83832be8d2ad3daef1
HyrQeilaunn <jongee1998@gmail.com>
2019-12-10 11:51:28 +0000

    Add files via upload

--- assets/js/vue.js
+++ assets/js/vue.js
@@ -5,6 +5,7 @@
 // Strings can be stored in Vue to easily edit an entire site for ease of access and editing without having to go through a HTML file looking for specific elements //
 // It makes updating content on websites much easier //
 // It also helps with translating sites since only this file would need to be handed over to a translator //
+// HTML Snippets are parsed as text rather than being processed as HTML //
 
 // Vue objects are structured similar to .JSON objects, which makes them easy to understand //
 
@@ -27,5 +28,14 @@ var footer = new Vue({
         events: "What's On",
         location: "How to Get Here",
         contact: "Contact Us"
+    }
+});
+
+var carousel = new Vue({
+    el: '#carousel',
+    data: {
+        drbrightconf: "Dr. Bright giving a conference talk",
+        drbridgeconf: "Dr. Bridge giving a conference talk",
+        fullconfhall: "A conference hall full of steadfast scientists"
     }
 });
