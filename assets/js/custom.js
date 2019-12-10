323aa04787dbb4f306a311e7195db762a7dfe7e6
HyrQeilaunn <jongee1998@gmail.com>
2019-12-10 12:45:59 +0000

    Add files via upload

---
+++ assets/js/custom.js
@@ -0,0 +1,4 @@
+//UWS1920 COMP10020 Internet Technologies Conference Web Application //
+// by B00346666 Jon A. Gilmour //
+
+// Custom.js contains all the custom JavaScript & jQuery functions required for the website //
